b793757e5cecdcc8e179885c465799e0d2c08bc1
Giorgio Costa <gegiorgio1991@gmail.com>
2020-05-17 19:43:55 -0300

    :pencil2: adicionando identity configurado para sqlite

---
+++ src/DevIO.App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+using System;
+using DevIO.App.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DevIO.App.Areas.Identity.IdentityHostingStartup))]
+namespace DevIO.App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
