987b4b55a00c0253d227a4b5dd7881809b401663
Giorgio Costa <gegiorgio1991@gmail.com>
2020-05-18 23:39:58 -0300

    :pencil2: irepository, iendereço, ifornecedores e iprodutos

---
+++ src/DevIO.Business/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
