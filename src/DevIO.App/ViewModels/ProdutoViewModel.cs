c18a5e99c55727db608e31a778264a95cd0324cc
Giorgio Costa <gegiorgio1991@gmail.com>
2020-05-22 23:16:09 -0300

    :pencil2: viewmodel

---
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace DevIO.App.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Descricao { get; set; }
+
+        public IFormFile ImagemUpload { get; set; }
+
+        public string Imagem { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int Valor { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+        [DisplayName("Ativo")]
+        public bool Ativo { get; set; }
+
+        /* EF relacionamento N para 1*/
+        public FornecedorViewModel Fornecedor { get; set; }
+    }
+}
