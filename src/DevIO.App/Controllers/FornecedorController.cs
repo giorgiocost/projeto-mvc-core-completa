5e9d86e9fbe313c00412abda2971439a9013ed1f
Giorgio Costa <gegiorgio1991@gmail.com>
2020-05-29 23:26:25 -0300

    :pencil2: index view fornecedor

--- src/DevIO.App/Controllers/FornecedorController.cs
+++ src/DevIO.App/Controllers/FornecedorController.cs
@@ -1,12 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DevIO.App.Areas.Identity.Data;
+using DevIO.App.ViewModels;
+using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
-namespace DevIO.App
+namespace DevIO.App.Controllers
 {
-    public class FornecedorController : Controller
+    public class FornecedoresController : Controller
     {
-        public IActionResult Index()
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+
+        // GET: Fornecedores
+        public async Task<IActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<FornecedorViewModel>> (await _fornecedorRepository.ObterTodos()));
+        }
+
+        // GET: Fornecedores/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _fornecedorRepository.ObterPorId(id)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedores/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Fornecedores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Fornecedor fornecedor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(fornecedor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedores/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.FornecedorViewModel.FindAsync(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+            return View(fornecedor);
+        }
+
+        // POST: Fornecedores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Fornecedor fornecedor)
+        {
+            if (id != fornecedor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fornecedor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FornecedorExists(fornecedor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedores/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.FornecedorViewModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
+        // POST: Fornecedores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var fornecedor = await _context.FornecedorViewModel.FindAsync(id);
+            _context.FornecedorViewModel.Remove(fornecedor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FornecedorExists(Guid id)
+        {
+            return _context.FornecedorViewModel.Any(e => e.Id == id);
         }
     }
 }
